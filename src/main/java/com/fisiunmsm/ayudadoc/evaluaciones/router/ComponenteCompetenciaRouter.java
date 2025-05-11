7debd2086a80140ce46613123dd2ee58de7af739
SergIOSanchez99 <sergio.sanchez3@unmsm.edu.pe>
2025-05-10 23:27:03 -0500

    Archivo subido para ejecucion del API.

---
+++ src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/router/ComponenteCompetenciaRouter.java
@@ -0,0 +1,25 @@
+package com.fisiunmsm.ayudadoc.evaluaciones.router;
+
+import com.fisiunmsm.ayudadoc.evaluaciones.handler.ComponenteCompetenciaHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+
+@Configuration
+public class ComponenteCompetenciaRouter {
+    private static final String PATH = "/api/componente-competencia";
+
+    @Bean
+    public RouterFunction<ServerResponse> componenteCompetenciaRoutes(ComponenteCompetenciaHandler handler) {
+        return RouterFunctions.route(GET(PATH), handler::findAll)
+                .andRoute(GET(PATH + "/{id}"), handler::findById)
+                .andRoute(GET(PATH + "/curso-competencia/{cursocompetenciaid}"), handler::findByCursocompetenciaid)
+                .andRoute(POST(PATH), handler::save)
+                .andRoute(PUT(PATH + "/{id}"), handler::update)
+                .andRoute(DELETE(PATH + "/{id}"), handler::delete);
+    }
+}
