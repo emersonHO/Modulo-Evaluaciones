24aa9eca353f51192011b0032e84c2248e7abc35
SergIOSanchez99 <sergio.sanchez3@unmsm.edu.pe>
2025-05-11 23:56:46 -0500

    Archivos subidos a carpeta router.

--- src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/router/ComponenteCompetenciaRouter.java
+++ src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/router/ComponenteCompetenciaRouter.java
@@ -1,6 +1,7 @@
 package com.fisiunmsm.ayudadoc.evaluaciones.router;
 
 import com.fisiunmsm.ayudadoc.evaluaciones.handler.ComponenteCompetenciaHandler;
+import com.fisiunmsm.ayudadoc.evaluaciones.handler.CompetenciaHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -14,12 +15,19 @@ public class ComponenteCompetenciaRouter {
     private static final String PATH = "/api/componente-competencia";
 
     @Bean
-    public RouterFunction<ServerResponse> componenteCompetenciaRoutes(ComponenteCompetenciaHandler handler) {
+    public RouterFunction<ServerResponse> componenteCompetenciaRoutes(ComponenteCompetenciaHandler handler,
+            CompetenciaHandler competenciaHandler) {
         return RouterFunctions.route(GET(PATH), handler::findAll)
                 .andRoute(GET(PATH + "/{id}"), handler::findById)
-                .andRoute(GET(PATH + "/curso-competencia/{cursocompetenciaid}"), handler::findByCursocompetenciaid)
+                .andRoute(GET(PATH + "/curso-competencia/{cursocompetenciaid}"),
+                        handler::findByCursocompetenciaid)
                 .andRoute(POST(PATH), handler::save)
                 .andRoute(PUT(PATH + "/{id}"), handler::update)
-                .andRoute(DELETE(PATH + "/{id}"), handler::delete);
+                .andRoute(DELETE(PATH + "/{id}"), handler::delete)
+                .andRoute(GET("/api/componente-competencia-detalle"), handler::findAllDetalles)
+                .andRoute(GET("/api/componente-competencia-con-competencias"),
+                        handler::findAllComponentesConCompetencias)
+                .andRoute(GET("/api/componentes-con-peso"), handler::findAllComponentesConPeso)
+                .andRoute(GET("/api/competencias"), competenciaHandler::findAll);
     }
 }
