a3af12c1bbf74e6293be0ba7bde482ef767239b1
SergioCallanPerez <153020034+SergioCallanPerez@users.noreply.github.com>
2025-05-16 00:56:02 -0500

    Deleting all rubrica files

--- src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/repository/RubricaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fisiunmsm.ayudadoc.evaluaciones.repository;
-
-import com.fisiunmsm.ayudadoc.evaluaciones.entity.Rubrica;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RubricaRepository extends ReactiveCrudRepository<Rubrica, Integer> {
-}
