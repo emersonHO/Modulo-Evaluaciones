82a3d53515d5f6c767e86c39dc79294228ec42da
SergioCallanPerez <153020034+SergioCallanPerez@users.noreply.github.com>
2025-07-02 00:37:17 -0500

    Revert "Minor test in SecurityConfig"
    This reverts commit 2c76c52949405a493cce27ea9a48f634212a2664.

--- src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/config/SecurityConfig.java
+++ src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
         return http
             .csrf(csrf -> csrf.disable())
             .authorizeExchange(exchange -> exchange
-                .pathMatchers("/api/**", "auth/login").permitAll()
+                .pathMatchers("/api/**").permitAll()
                 .anyExchange().authenticated()
             )
             .build();
