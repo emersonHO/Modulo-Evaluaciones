03d7cf5dc8e157f76933d0f7e27dc40e8ffdb535
Piero Rojas <133281510+RiotRed@users.noreply.github.com>
2025-07-08 22:04:24 -0500

    cambio para AllowedOrigins

--- src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/config/CORSGlobalConfig.java
+++ src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/config/CORSGlobalConfig.java
@@ -14,7 +14,7 @@ public class CORSGlobalConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5176"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
