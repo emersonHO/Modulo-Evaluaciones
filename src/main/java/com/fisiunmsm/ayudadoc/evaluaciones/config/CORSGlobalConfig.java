d90370e4e8d1322f92180178649cdbcd03662d10
SergioCallanPerez <153020034+SergioCallanPerez@users.noreply.github.com>
2025-07-11 01:24:28 -0500

    Adding the azure link in CORS

--- src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/config/CORSGlobalConfig.java
+++ src/main/java/com/fisiunmsm/ayudadoc/evaluaciones/config/CORSGlobalConfig.java
@@ -14,7 +14,10 @@ public class CORSGlobalConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "https://calm-flower-02043d00f.1.azurestaticapps.net"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
